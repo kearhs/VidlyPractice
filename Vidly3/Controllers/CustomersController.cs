23f5e6231c5e33a1b5ea4e312cc1a0ce5d0545a2
Kearhs <david.jon.henley@gmail.com>
2020-04-23 15:40:13 -0600

    Initial Commit

---
+++ Vidly3/Controllers/CustomersController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Ajax.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly3.Models;
+using Vidly3.ViewModels;
+
+namespace Vidly3.Controllers
+{
+    public class CustomersController : Controller
+    {
+        // Declare Customers Here Since No DB
+        public List<Customer> customers = new List<Customer>
+        {
+            new Customer { Id = 1, Name = "John Smith" },
+            new Customer { Id = 2, Name = "Mary Williams" }
+        };
+
+        // GET: Customers
+        public ActionResult Index()
+        {
+            var customerViewModel = new CustomerViewModel
+            {                
+                Customers = customers
+            };
+
+            return View(customerViewModel);
+        }
+
+        [Route("Customers/Details/{customerId}")]
+        public ActionResult Details(int customerId)
+        {
+            var customer = customers.FirstOrDefault(c => c.Id == customerId);
+
+            if (customer == null)
+                return HttpNotFound();
+
+            return View(customer);
+        }
+    }
+}
---
+++ Vidly3/Controllers/MoviesController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Vidly3.Models;
+using Vidly3.ViewModels;
+
+namespace Vidly3.Controllers
+{
+    public class MoviesController : Controller
+    {
+        public List<Movie> movies = new List<Movie>
+        {
+            new Movie { Name = "Shrek" },
+            new Movie { Name = "Wall-e" }
+        };
+
+        // GET: Movies/Random
+        public ActionResult Random()
+        {
+            var movie = new Movie { Name = "Shrek!" };
+            var customers = new List<Customer>
+            {
+                new Customer { Name = "Customer 1" },
+                new Customer { Name = "Customer 2" }
+            };
+            var viewModel = new RandomMovieViewModel
+            {
+                Movie = movie,
+                Customers = customers
+            };
+
+            return View(viewModel);
+        }
+
+        public ActionResult Index()
+        {
+            var movieViewModel = new MovieViewModel
+            {
+                Movies = movies
+            };
+
+            return View(movieViewModel);
+        }
+    }
+}
