f427c933db9ae801977f1899e8060c4fd5671c6f
David Henley <David.Jon.Henley@gmail.com>
2020-04-27 13:57:51 -0600

    Finish Section 8

---
+++ Vidly3/Models/ForgotPasswordViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly3.Models
+{
+    public class ForgotPasswordViewModel
+    {
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
---
+++ Vidly3/Models/ForgotViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Vidly3.Models
+{
+    public class ForgotViewModel
+    {
+        [Required]
+        [Display(Name = "Email")]
+        public string Email { get; set; }
+    }
+}
